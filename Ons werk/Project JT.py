496b30a846206e76addd5b5202a3d7ded6d461b8
Jouke Tuinstra <99037270@mydavinci.nl>
2022-11-26 01:05:06 +0100

    Boodschappenlijstje is voor nu goed, niks toegevoegd qua deze zit al in het boodschappenlijstje,
    Homepage toegevoegd, met tot nu toe alleen het boodschappenlijsjte.

--- Ons werk/Project JT.py
+++ Ons werk/Project JT.py
@@ -1,50 +1,68 @@
 from ctypes import windll
 from tkinter import *
 
-producten = []
-
 windll.shcore.SetProcessDpiAwareness(1)
-window = Tk()
-window.title('Boodschappenlijstje')
-window.geometry('500x600')
-window.configure(background='#121212')
+homepage=Tk()
+homepage.geometry('700x700')
+homepage.title('Homepage')
+homepage.configure(background='#121212')
 
-entry = Entry(window, font='Arial 20 bold', width=13)
-entry.place(x=60, y=10)
+def boodschappenlijstje():
+    producten = []
 
-lijn = Label(window, text='-' * 73, background='#121212', foreground='#F0F8FF')
-lijn.place(x=25, y=60)
+    windll.shcore.SetProcessDpiAwareness(1)
+    window = Tk()
+    window.title('Boodschappenlijstje')
+    window.geometry('500x600')
+    window.configure(background='#121212')
 
-eersteY = 90
-nieuweX = 35
-testX = 0
-if testX == 9:
-    nieuweX = 350
+    entry = Entry(window, font='Arial 20 bold', width=13)
+    entry.place(x=60, y=10)
+
+    lijn = Label(window, text='-' * 73, background='#121212', foreground='#F0F8FF')
+    lijn.place(x=25, y=60)
+
     eersteY = 90
-
-
-def antwoordinscherm():
-    global eersteY
-    global testX
-    global nieuweX
-    global producten
-    if testX == 10:
-        nieuweX = 250
+    nieuweX = 35
+    testX = 0
+    count = 1
+    if testX == 9:
+        nieuweX = 350
         eersteY = 90
-    antwoord = entry.get()
-    if antwoord in producten:
-        print('Zit al in')
-    else:
-        producten.append(antwoord)
-        print('Zit nu wel in')
-    entryinscreen = Label(window, text='1 x ' + antwoord, font='arial 20 bold', background='#121212',
-                          foreground='#F0F8FF')
-    entryinscreen.place(x=nieuweX, y=eersteY)
-    eersteY += 50
-    testX += 1
 
 
-submit = Button(window, text='Submit', font='Arial 13 bold', background='#F0F8FF', command=antwoordinscherm)
-submit.place(x=315, y=10)
+    def antwoordinscherm():
+        global eersteY
+        global testX
+        global nieuweX
+        global producten
+        global count
+        if testX == 10:
+            nieuweX = 250
+            eersteY = 90
+        antwoord = entry.get()
+        if antwoord in producten:
+            print('Zit al in')
 
-window.mainloop()
+        else:
+            producten.append(antwoord)
+            print('Zit nu wel in')
+        entryInScreen = Label(window,
+                              text='1 x ' + antwoord,
+                              font='arial 20 bold',
+                              background='#121212',
+                              foreground='#F0F8FF')
+        entryInScreen.place(x=nieuweX, y=eersteY)
+        eersteY += 50
+        testX += 1
+
+
+    submit = Button(window, text='Submit', font='Arial 13 bold', background='#F0F8FF', command=antwoordinscherm)
+    submit.place(x=315, y=10)
+
+    window.mainloop()
+
+knopBoodLijst=Button(homepage,text='Boodschappenlijst',font='Arial 20 bold',width=35,command=boodschappenlijstje)
+knopBoodLijst.pack()
+
+homepage.mainloop()
