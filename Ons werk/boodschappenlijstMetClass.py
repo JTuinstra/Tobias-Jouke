a5c0cd2964057f609460898db345144c4c28799f
Jouke Tuinstra <99037270@mydavinci.nl>
2022-11-27 15:52:55 +0100

    boodschappenlijstMetClass klaar.

--- Ons werk/boodschappenlijstMetClass.py
+++ Ons werk/boodschappenlijstMetClass.py
@@ -1,5 +1,6 @@
 from ctypes import windll
 from tkinter import *
+from tkinter import messagebox
 
 windll.shcore.SetProcessDpiAwareness(1)
 
@@ -9,7 +10,9 @@ class Boodschappenlijst:
     def __init__(self):
         self.inlijst = []
         self.plaatsX = 50
-        self.plaatsY = 110
+        self.plaatsY = 60
+        self.countFornextRow = 0
+        self.productCount = 1
 
         self.window = Tk()
         self.window.title('Boodschappenlijst')
@@ -18,25 +21,44 @@ class Boodschappenlijst:
 
         ##############################
 
+        self.window.bind('<Return>', self.antwoord)
         self.submit = Button(self.window, text='Submit', font='Arial 12 bold', command=self.antwoord)
         self.entry = Entry(self.window, width=20, font='Arial 15 bold')
         self.lijn = Label(self.window, text='-' * 75, background='#121212', foreground='white')
-        self.getInBeeld = Label(self.window,text=self.entry.get(), background='#121212', foreground='white', font='Arial 15 bold')
 
     def plaaten(self):
         self.submit.place(x=350, y=25)
         self.entry.place(x=45, y=28)
         self.lijn.place(x=20, y=85)
-        self.getInBeeld.place(x=self.plaatsX, y=self.plaatsY)
         self.window.mainloop()
 
-    def antwoord(self):
-        if self.entry.get() in self.inlijst:
-            pass
+    def antwoord(self, event=None):
+        if len(self.entry.get()) <= 11:  #<----checking if length is too long
 
+            if self.countFornextRow == 10:  # <-----positioning
+                self.plaatsX = 270
+                self.plaatsY = 110
+
+            # If statements voor wat er in entry zit
+            if self.entry.get() in self.inlijst:
+                self.countFornextRow = self.countFornextRow
+                self.productCount += 1
+
+
+            else:
+                self.inlijst.append(self.entry.get())
+                self.plaatsY += 50
+                self.countFornextRow += 1
+                self.productCount = 1
+            # If statements voor wat er in entry zit
+
+            self.getInBeeld = Label(self.window, text=str(self.productCount) + ' x ' + self.entry.get(),
+                                    background='#121212', foreground='white', font='Arial 15 bold')
+            self.getInBeeld.place(x=self.plaatsX, y=self.plaatsY)
 
         else:
-            self.inlijst.append(self.entry.get())
+            messagebox.showinfo(title='Te lang', message='Dit woord is te lang.')
+
 
 boodlijst = Boodschappenlijst()
 boodlijst.plaaten()
