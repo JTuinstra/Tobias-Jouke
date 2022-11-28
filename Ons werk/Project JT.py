a5e59ebdca8845562d3abe9cee40182828ec7adc
Jouke Tuinstra <99037270@mydavinci.nl>
2022-11-28 14:20:07 +0100

    Exit aan boodschappenlijstje toegevoegd en ook aan hackpaleis uitloggen dat ie dan weer naar mainscreen gaat. Ik heb ook een fix als ie python interpreter vraagt

--- Ons werk/Project JT.py
+++ Ons werk/Project JT.py
@@ -5,31 +5,30 @@ from tkinter import messagebox
 
 windll.shcore.SetProcessDpiAwareness(1)
 
-def homepage():
+
+def mainPage():
     homepage = Tk()
     homepage.geometry('700x700')
     homepage.title('Homepage')
     homepage.configure(background='#121212')
 
-
     ########################################################
     # CHEATPALEIS!
     ########################################################
     def hackPaleis():
-        window = Tk()
-        window.title('KOVAAKS INTO HACK')
-        window.geometry('700x700')
+        homepage.title('KOVAAKS INTO HACK')
+        homepage.title('HackPaleis')
 
-        scherm = Frame(window, width=700, height=700, background='red')
+        scherm = Frame(homepage, width=700, height=700, background='red')
         scherm.pack()
 
-        frame = Frame(window, width=600, height=600, background='dark red')
+        frame = Frame(homepage, width=600, height=600, background='dark red')
         frame.place(x=50, y=50)
 
-        def login():
-            username = 'tobias'
+        def login(event):
+            username = ''
             capital = 'Tobias'
-            password = 'wacht'
+            password = ''
 
             if enterUser.get() == username or capital and enterPass.get() == password:
                 messagebox.showinfo(title='VALID', message='LOGIN SUCCESVOL')
@@ -42,7 +41,7 @@ def homepage():
                 enterPass.destroy()
                 submit.destroy()
 
-                logtitel = Label(window, text='CHEATPALEIS VAN TOBIAS', font='Papyrus 23 bold', background='dark red')
+                logtitel = Label(homepage, text='CHEATPALEIS VAN TOBIAS', font='Papyrus 23 bold', background='dark red')
                 logtitel.place(x=57, y=75)
 
                 def injectedcsgo(injectcsgo=False):
@@ -73,38 +72,38 @@ def homepage():
                         time.sleep(1)
                         messagebox.showinfo(title='DONE', message='SUCCESFULLY INJECTED')
 
-                csgo = Label(window, font='Arial 20 bold', text='CSGO', background='dark red')
+                csgo = Label(homepage, font='Arial 20 bold', text='CSGO', background='dark red')
                 csgo.place(x=100, y=275)
 
-                injectcsgo = Button(window, text='Inject CSGO hack', font='Arial 10 bold', background='dark red',
+                injectcsgo = Button(homepage, text='Inject CSGO hack', font='Arial 10 bold', background='dark red',
                                     activebackground='red', command=lambda: injectedcsgo(True))
                 injectcsgo.place(x=300, y=275)
 
-                valorant = Label(window, text='VALORANT', font='Arial 20 bold', background='dark red')
+                valorant = Label(homepage, text='VALORANT', font='Arial 20 bold', background='dark red')
                 valorant.place(x=100, y=375)
 
-                injectvalo = Button(window, text='Inject VALO hack', font='Arial 10 bold', background='dark red',
+                injectvalo = Button(homepage, text='Inject VALO hack', font='Arial 10 bold', background='dark red',
                                     activebackground='red', command=lambda: injectedvalo(True))
                 injectvalo.place(x=300, y=375)
 
-                teris = Label(window, text='TETRIS.IO', font='Arial 20 bold', background='dark red')
+                teris = Label(homepage, text='TETRIS.IO', font='Arial 20 bold', background='dark red')
                 teris.place(x=100, y=475)
 
-                injecttetris = Button(window, text='Inject TETRIS hack', font='Arial 10 bold', background='dark red',
+                injecttetris = Button(homepage, text='Inject TETRIS hack', font='Arial 10 bold', background='dark red',
                                       activebackground='red', command=lambda: injectedtetr(True))
                 injecttetris.place(x=300, y=475)
 
-                letter = Label(window, text='LETTERLEAGUE', font='Arial 20 bold', background='dark red')
+                letter = Label(homepage, text='LETTERLEAGUE', font='Arial 20 bold', background='dark red')
                 letter.place(x=100, y=575)
 
-                injectletter = Button(window, text='Inject LETTER LEAGUE hack', font='Arial 10 bold',
+                injectletter = Button(homepage, text='Inject LETTER LEAGUE hack', font='Arial 10 bold',
                                       background='dark red',
                                       activebackground='red', command=lambda: injectedletter(True))
                 injectletter.place(x=400, y=575)
 
-                logout = Button(window, text='Uitloggen', font='Arial 20 bold', background='red',
+                logout = Button(homepage, text='Uitloggen', font='Arial 20 bold', background='red',
                                 activebackground='red',
-                                command=lambda: [window.destroy(), hackPaleis()])
+                                command=lambda: [homepage.destroy(), mainPage()])
                 logout.place(x=400, y=150)
 
 
@@ -112,26 +111,26 @@ def homepage():
             else:
                 messagebox.showerror(title='ERROR', message='FOUTE GEGEVENS')
 
-        titel = Label(window, text='HACKS VAN TOBIAS', font='Papyrus 25 bold', background='dark red')
+        titel = Label(homepage, text='HACKS VAN TOBIAS', font='Papyrus 25 bold', background='dark red')
         titel.place(x=70, y=75)
 
-        usernameLabel = Label(window, text='Username:', font='Arial 20 bold', background='dark red')
+        usernameLabel = Label(homepage, text='Username:', font='Arial 20 bold', background='dark red')
         usernameLabel.place(x=175, y=200)
 
-        passwordLabel = Label(window, text='Password:', font='Arial 20 bold', background='dark red')
+        passwordLabel = Label(homepage, text='Password:', font='Arial 20 bold', background='dark red')
         passwordLabel.place(x=175, y=300)
 
-        enterUser = Entry(window, font='Arial 20 bold', background='red', foreground='dark red')
+        enterUser = Entry(homepage, font='Arial 20 bold', background='red', foreground='dark red')
         enterUser.place(x=175, y=250)
 
-        enterPass = Entry(window, font='Arial 20 bold', background='red', foreground='dark red', show='*')
+        enterPass = Entry(homepage, font='Arial 20 bold', background='red', foreground='dark red', show='*')
         enterPass.place(x=175, y=350)
 
-        submit = Button(window, text='Submit', font='Arial 20 bold', background='dark red', activebackground='red',
-                        command=login)
+        submit = Button(homepage, text='Submit', font='Arial 20 bold', background='dark red', activebackground='red',
+                        command=lambda: login(''))
         submit.place(x=175, y=400)
 
-        window.mainloop()
+        homepage.bind('<Return>', login)
 
     ########################################################
     # CHEATPALEIS!
@@ -144,8 +143,7 @@ def homepage():
 
         def __init__(self):
             self.arrow = PhotoImage(file='arrow.png')
-            self.arrowKnop=Button(homepage,image=self.arrow)
-            self.arrowKnop.pack()
+            self.arrowKnop = Button(homepage, image=self.arrow)
             self.inlijst = []
             self.plaatsX = 50
             self.plaatsY = 60
@@ -160,11 +158,13 @@ def homepage():
             self.submit = Button(homepage, text='Submit', font='Arial 12 bold', command=self.antwoord)
             self.entry = Entry(homepage, width=20, font='Arial 15 bold')
             self.lijn = Label(homepage, text='-' * 75, background='#121212', foreground='white')
+            self.exitKnop = Button(homepage, text='Exit', font='arial 10 bold',command=lambda: [homepage.destroy(),mainPage()])
 
         def plaaten(self):
             self.submit.place(x=350, y=25)
             self.entry.place(x=45, y=28)
             self.lijn.place(x=20, y=85)
+            self.exitKnop.place(x=445,y=30)
 
         def antwoord(self, event=None):
             if len(self.entry.get()) <= 11:  # <----checking if length is too long
@@ -207,11 +207,16 @@ def homepage():
         boodlijst = Boodschappenlijst()
         boodlijst.plaaten()
 
+    def callHackPaleis():
+        knopBoodLijst.destroy()
+        tobiasHackerPaleis.destroy()
+        hackPaleis()
+
     knopBoodLijst = Button(homepage, text='Boodschappenlijst', font='Arial 20 bold', width=35, command=callBoodlijst)
     knopBoodLijst.pack()
 
     tobiasHackerPaleis = Button(homepage, text='Tobias Hack Paleis', font='Arial 20 bold', width=35,
-                                background='light blue', command=hackPaleis, )
+                                background='light blue', command=callHackPaleis, )
     tobiasHackerPaleis.pack()
 
     ########################################################
@@ -221,4 +226,4 @@ def homepage():
     homepage.mainloop()
 
 
-homepage()
+mainPage()
