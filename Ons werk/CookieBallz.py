ca4de19b95acc9f34b44236b5f12cc6983625915
Jouke Tuinstra <99037270@mydavinci.nl>
2022-12-13 13:18:15 +0100

    NIET PULLEN! dubbelcoins toegevoegd en werkt nog niet top door error

--- Ons werk/CookieBallz.py
+++ Ons werk/CookieBallz.py
@@ -9,6 +9,7 @@ class CokkieBalzz:
         self.window.title('Cockieball')
 
         self.coinsPerClick = 1
+        self.DoubleCost=10
         self.balance = 0
         self.cookieImage = PhotoImage(file='cookie.png')
         self.shopicon = PhotoImage(file='shopicon.png')
@@ -30,11 +31,16 @@ class CokkieBalzz:
                              borderwidth=0,
                              activebackground='white', command=self.click)
 
+        self.dubbelCoins = Button(self.window,text='Double Coins',font='Arial 15 bold',
+                                  borderwidth=0,
+                                  activebackground='white',command=lambda: self.aankoop(self.DoubleCost))
+        self.costdubbel=Label(self.window,text='Cost: '+str(self.DoubleCost),background='white',font='Arial 15 bold')
+
         self.shopScherm = Canvas(self.window, height=600, width=500, background='white')
 
         self.prijzen = {
             'snor': 5
-            #Hier dus alle andere dingen die je kan kopen.
+            # Hier dus alle andere dingen die je kan kopen.
         }
         #####################################################################################################
         # KNOPPEN & LABELS
@@ -45,8 +51,10 @@ class CokkieBalzz:
     def plaatsen(self):
         self.cookie.place(x=100, y=125)
         self.shopKnop.place(x=400, y=500)
+        self.dubbelCoins.place(x=50, y=500)
 
         self.balanceOnScreen.pack(pady=30)
+        self.costdubbel.place(x=50,y=550)
 
         self.window.mainloop()
 
@@ -56,20 +64,31 @@ class CokkieBalzz:
 
     def shop(self):
         self.shopScherm.pack()
-        self.snor.place(x=90, y=90)
+        self.snor.place(x=90, y=200)
+
+    def removing(self):
+        self.makingSure.destroy()
+        self.dubbelCoins.place(x=50, y=525)
 
     def aankoop(self, amountOfCoins):
-        makingSure = Frame(self.window, highlightbackground="black", highlightthickness=5, height=200, width=250,
-                           bg='white', bd=5)
-        makingSure.place(y=150, x=150)
-        notEnoughCoins = Label(makingSure, background='white',
-                               text=f"You don't have enough coins to buy that.\n\nThis costs {amountOfCoins}")
-        areYouSure = Label(makingSure, background='white', text='Are you sure you want to buy that?')
+        self.makingSure = Frame(self.window, highlightbackground="black", highlightthickness=5, height=200, width=250,
+                                bg='white', bd=5)
+        self.makingSure.place(y=150, x=150)
 
-        yes = Button(makingSure, text='Yes', bg='green', command=lambda: ifPressedYes())
-        no = Button(makingSure, text='no', bg='red', command=lambda: makingSure.destroy())
-        ok = Button(makingSure, text='ok', background='green', height=50, width=50,
-                    command=lambda: makingSure.destroy())
+        notEnoughCoins = Label(self.makingSure, background='white',
+                               text=f"You don't have enough coins to buy that.\n\nThis costs {amountOfCoins}")
+        areYouSure = Label(self.makingSure, background='white', text='Are you sure you want to buy that?')
+
+
+        ###############################################################################
+        yes = Button(self.makingSure, text='Yes', bg='green', command=lambda: ifPressedYes())
+        no = Button(self.makingSure, text='no', bg='red', command=self.removing)
+        ok = Button(self.makingSure, text='ok', background='green', height=50, width=50,
+                    command=self.removing)
+        ###############################################################################
+
+        if amountOfCoins==self.DoubleCost:
+            self.dubbelCoins.destroy()
 
         if amountOfCoins <= self.balance:
             areYouSure.place(x=10, y=10)
@@ -78,12 +97,16 @@ class CokkieBalzz:
 
             def ifPressedYes():
                 self.balance -= amountOfCoins
+                self.coinsPerClick *= 2
                 self.balanceOnScreen.configure(text=self.balance)
-                makingSure.destroy()
+                self.makingSure.destroy()
+                self.dubbelCoins.place(x=50,y=525)
 
         else:
             notEnoughCoins.place(x=10, y=10)
             ok.place(x=100, y=100, height=50, width=50)
+
+
 
 
 clicker = CokkieBalzz()
