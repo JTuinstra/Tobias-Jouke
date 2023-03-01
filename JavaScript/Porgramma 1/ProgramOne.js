e3eaaa4dc85830be59d7cfa5b3413e0c9d6b3fa9
Gert <43937531+HeiligeKoe@users.noreply.github.com>
2023-03-01 22:58:18 +0100

    Poep in je kaas

--- JavaScript/Porgramma 1/ProgramOne.js
+++ JavaScript/Porgramma 1/ProgramOne.js
@@ -2,13 +2,19 @@ function easy() {
     document.getElementById('diff').style.scale = 0;
 
     for (let i = 1; i < 12; i++) {
+        let count = 0
         let container = document.createElement('div');
-
         let button = document.createElement('button');
+
         button.id = 'button' + i;
         button.innerHTML = '?';
         button.onclick = function () {
             button.innerHTML = i;
+            alert(random_number)
+        }
+
+        if (count % 2 == 0) {
+            button.innerHTML = Math.floor(Math.random * 100)
         }
 
         document.body.appendChild(container);
